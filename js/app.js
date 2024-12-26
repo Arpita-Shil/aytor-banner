9e34b7d50bf2b963f54fe9a275436dc31a0eff03
Arpita-Shil <arpitashilctg@gmail.com>
2024-12-27 01:11:57 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.sliders').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      dots: true,
+      dotsClass: "container test"
+    });
+  });
+          
